62ad0e60bcc42a706b01db9425ddb8992a850288
felp302 <felp302@gmail.com>
2015-09-08 18:09:25 +0100

    Update footer.php

--- application/views/citylight/footer.php
+++ application/views/citylight/footer.php
@@ -96,10 +96,11 @@
   <script type="text/javascript" src="<?php echo $this->config->base_url(); ?>js/jquery.parallax-1.1.3.js"></script>
   <script type="text/javascript" src="<?php echo $this->config->base_url(); ?>js/SmoothScroll.js"></script>
   <script type="text/javascript" src="<?php echo $this->config->base_url(); ?>js/lock.min.js"></script>
+  <script type="text/javascript" src="<?php echo $this->config->base_url(); ?>js/unveil.js"></script>
   <!-- THEME SCRIPT -->
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhqHhs7yOY46r2H-71JhTA8dGorPqIu30"></script>
   <script type="text/javascript" src="<?php echo $this->config->base_url(); ?>js/script.js"></script>
-  <script src="//my.hellobar.com/6049964f0fdcb99f5f1918dc0ebaecdebb0dae92.js" type="text/javascript" charset="utf-8" async="async"></script>
-  
+ <!-- <script src="//my.hellobar.com/6049964f0fdcb99f5f1918dc0ebaecdebb0dae92.js" type="text/javascript" charset="utf-8" async="async"></script>
+  -->
 </body>
 </html>
