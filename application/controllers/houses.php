00ca8c9a403da4b347b2180fcee8716cae725f44
felp302 <felp302@gmail.com>
2015-09-09 13:55:54 +0100

    Update houses.php

--- application/controllers/houses.php
+++ application/controllers/houses.php
@@ -11,7 +11,7 @@ class Houses extends CI_Controller {
 	}
 	
 	public function returngooglelatlong($query){
-		$this->benchmark->mark('googlelatlong_start');
+
 		$search_code = urlencode($query);
 		if($res = $this->Housesm->verifysearch($search_code)){
 			
@@ -35,7 +35,7 @@ class Houses extends CI_Controller {
 				$this->Housesm->insertlatlong($search_code, $lat, $lng, $formatted_address, $type, $components);
 				
 				$res2 = $this->Housesm->verifysearch($search_code);
-				$this->benchmark->mark('search_end');
+
 				return $res2;
 				
 			}else
@@ -47,7 +47,7 @@ class Houses extends CI_Controller {
 	public function general_search($type = "sale"){
 		
 		$this->output->enable_profiler(TRUE);
-		$this->benchmark->mark('generarsearch_start');
+		
 		if($this->input->get('search') && (int)$this->input->get('search') >= 0 && $this->input->get('search') != ""){
 			$searchvalue = $this->input->get('search');
 			$cleanurl = false;
@@ -64,13 +64,13 @@ class Houses extends CI_Controller {
 		}else{
 			$this->makesearch($cleanurl, $searchvalue, $type);
 		}
-		$this->benchmark->mark('generalsearch_end');
+
 				
 	}
 
 
 	public function makesearch($cleanurl = false, $searchvalue,$type){
-		$this->benchmark->mark('search_start');
+
 		$filters = $this->getfilters();
 		if($this->input->get('page') && (int)$this->input->get('page') != 0){$page = (int)$this->input->get('page');}else{$page = 1;}
 		
@@ -161,7 +161,7 @@ class Houses extends CI_Controller {
 	      	$this->load->view('citylight/lists', $data);		
 		$this->getFooter();
 		$this->Housesm->savesearch($data['searchvalue'], $type);
-		$this->benchmark->mark('search_end');
+
 	}
 	public function getBread($details,$typesale,$actual){
 		if($typesale == 'sale'){
@@ -284,9 +284,9 @@ class Houses extends CI_Controller {
 
 	}
 	public function for_sale(){
-		$this->benchmark->mark('forsale_start');
+
 		$this->general_search("sale");
-		$this->benchmark->mark('forsale_end');
+
 	}
 	public function to_rent(){
 		
