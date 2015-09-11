78cc7847c67fa494df0ee49f1ed6535afe1c82c3
felp302 <felp302@gmail.com>
2015-09-11 11:16:54 +0100

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,8 +1,6 @@
 <footer>
 <div class="box">
     <br>
-    <h3 style="text-align:left;padding-left:30px;">Popular locations with houses for sale in the United Kingdom:</h3>
-    <br>
     <p>
         <a href="<?php echo $this->config->base_url(); ?>houses/uk.html" alt="More than 200,000 houses for sale in the United Kingdom" title="Houses for sale in the United Kingdom"><strong>More than 200,000 houses for sale in the United Kingdom</strong></a>
         <?php foreach($links as $link){
