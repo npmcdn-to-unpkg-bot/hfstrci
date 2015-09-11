c88f330b7e97ca6dd9d3809c44a71d2d7e5c7638
felp302 <felp302@gmail.com>
2015-09-11 10:20:05 +0100

    Update longform.php

--- application/views/longform.php
+++ application/views/longform.php
@@ -2,14 +2,15 @@
         <h1><a href="<?php echo $this->config->base_url(); ?>" title="HOUSES for SALE & to RENT"><img src="<?php echo $this->config->base_url(); ?>images/houses-for-sale-to-rent.png" alt="HOUSES for SALE & to RENT" title="HOUSES for SALE & to RENT"/></a></h1>
     </header>
    <form name="searchHouses" action="<?php echo $this->config->base_url();?>houses/for-sale/index.html" method="get" id="sale" class="search">
-           <form name="searchHouses" action="<?php echo $this->config->base_url();?>houses/to-rent/index.html" method="get" id="rent" class="search">
+           
      
       <fieldset>
           <h2>Find houses and properties for sale or to rent in the United Kingdom.</h2>
-          <input type="search" name="houses" id="search1" placeholder="Search for address, town or area..." />
+          <input type="search" name="search" id="search1" placeholder="Search for address, town or area..." form="sale" />
           <input type="submit" value="For Sale" name="action" id="forsale" class="button-submit" form="sale" />
           <input type="submit" value="To Rent" name="action" id="torent" class="button-submit" form="rent" />
       </fieldset>
-      
        </form>
+    <form name="searchrentHouses" action="<?php echo $this->config->base_url();?>houses/to-rent/index.html" method="get" id="rent" class="search">
+        <input type="hidden" name="search" form="rent" id="rentsearch" />
     </form>
