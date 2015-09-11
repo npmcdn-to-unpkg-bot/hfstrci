d4ac283953ca2635e858c46ba669c01fb3909eaf
felp302 <felp302@gmail.com>
2015-09-11 10:46:58 +0100

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,6 +2,11 @@
 <div class="box">
     <p>
         <a href="<?php echo $this->config->base_url(); ?>houses/uk.html" alt="More than 200,000 houses for sale in the United Kingdom" title="More than 200,000 houses for sale in the United Kingdom">More than 200,000 houses for sale in the United Kingdom</a>
+        <?php foreach($links as $link){
+            
+              echo ' <a href="'.$link->link.'">'.$link->title.'</a>';
+
+        }    ?>
         </p>
 </div>
 
@@ -10,18 +15,7 @@
         <a href="#" title="Houses for sale to rent on Facebook.">
             <img src="<?php echo $this->config->base_url(); ?>images/fb.png" alt="Houses for sale to rent on Facebook." title="Houses for sale to rent on Facebook.">
         </a>
-        <a href="#" title="Houses for sale to rent on Twitter.">
-            <img src="<?php echo $this->config->base_url(); ?>images/twitter.png" alt="Houses for sale to rent on Twitter." title="Houses for sale to rent on Twitter.">
-        </a>
-        <a href="#" title="Houses for sale to rent on Google +.">
-            <img src="<?php echo $this->config->base_url(); ?>images/g+.png" alt="Houses for sale to rent on Google +." title="Houses for sale to rent on Google +.">
-        </a>
-        <a href="#" title="Houses for sale to rent Contact form.">
-            <img src="<?php echo $this->config->base_url(); ?>images/mail.png" alt="Houses for sale to rent contact form." title="Houses for sale to rent contact form.">
-        </a>
     </div>
-    <p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
-    </footer>
-   
+</footer>
 </body>
 </html>
