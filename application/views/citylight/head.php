a1150f26c5ff7f16e96d4f700345e7fc67e82671
felp302 <felp302@gmail.com>
2015-09-14 16:12:36 +0100

    Update head.php

--- application/views/citylight/head.php
+++ application/views/citylight/head.php
@@ -1,11 +1,9 @@
 <!doctype html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <title><?php echo $title; ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
   <meta name="author" content="Houses for Sale & to Rent">
   <?php
   if(isset($canonical))
@@ -19,38 +17,28 @@
   if(isset($next))
   	echo '<link rel="next" href="'.$next.'" />';  	
   ?>
-
   <link rel="shortcut icon" href="images/icon/favicon.jpg" type="image/x-icon">
-
-  <?php if(isset($meta)){echo $meta;} ?>
-  <?php //echo $js; ?>
-
+  <?php if(isset($meta)){
+  	foreach($meta as $met){
+  		echo '<meta name="'.$met["name"].'" content="'.$met["content"].'">';
+  	}
+  } ?>
   <!-- GOOGLE WEB FONTS INCLUDE -->
   <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,300italic,400italic' rel='stylesheet' type='text/css'>
-
   <!-- STYLESHEETS -->
   <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>css/citylight/bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>css/citylight/bootstrap-theme.min.css">
   <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>css/citylight/font-awesome.min.css">
-
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
-	      type="text/css" rel="stylesheet" />
-	       
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
   <!-- THEME STYLESHEETS -->
   <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>css/citylight/style.css">
   <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>css/citylight/shortcode.css">
   <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>css/citylight/citilights-shortcode.css">
   <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>css/citylight/color/color1.css">
-  
-
-  
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-  
-
-  
-</head>
+  <![endif]--> 
+  </head>
