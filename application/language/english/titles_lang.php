9f3df2be28d0ae961b1a2e14116c472a3948c5c2
felp302 <felp302@gmail.com>
2015-09-15 11:38:09 +0100

    Update titles_lang.php

--- application/language/english/titles_lang.php
+++ application/language/english/titles_lang.php
@@ -7,13 +7,15 @@ $lang["uk-title"] = 'Houses for Sale & to Rent, Houses in the United Kingdom';
 $lang["price-title"] = 'House for Sale & to Rent in {$searchvalue}';
 
 $lang["price-meta"] = '
-<meta name="description" content="Houses for sale in {$location}, Houses to rent in {$location}, Flats in {$location}, Properties for sale in {$location}, Properties to rent in {$location}" />
+<meta name="description" content="{$cnum} Houses for sale in {$location}{$postcode}, Houses to rent in {$location}. Best Properties in {$location} you find here." />
 <meta name="keywords" content="Houses for sale in {$location}, {$location}, Houses to rent in {$location}, Flats in {$location}, Houses for sale & to rent, Properties for sale in {$location}, Properties to rent in {$location}" />
 <meta name="geo.placename" content="{$placename}" />
-<meta name="robots" content="noindex, follow" />
 <meta name="geo.region" content="{$region}" />
 <meta name="geo.position" content="{$lat};{$lng}" />
 <meta name="ICBM" content="{$lat}, {$lng}" />
+<meta property=”og:title” content=”House for Sale & to Rent in {$location}”/>
+<meta property=”og:type” content=”website”/>
+<meta property=”og:description” content=”{$cnum} Houses for sale in {$location}{$postcode}, Houses to rent in {$location}. Best Properties in {$location} you find here.”/>
 ';
 
 $lang["paginator-firstpage"] = "First Page";
