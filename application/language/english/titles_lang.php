0f688cb9eb6a099f517c688f4744cd18cee0afb3
felp302 <felp302@gmail.com>
2015-09-15 11:57:42 +0100

    Update titles_lang.php

--- application/language/english/titles_lang.php
+++ application/language/english/titles_lang.php
@@ -15,8 +15,32 @@ $lang["price-meta"] = '
 <meta name="ICBM" content="{$lat}, {$lng}" />
 <meta property=”og:title” content=”House for Sale & to Rent in {$location}”/>
 <meta property=”og:type” content=”website”/>
+<meta property=”og:description” content=”{$cnum} Houses for sale in {$location}{$postcode}, Houses to rent in {$location}. The est Properties in {$location} you find here.”/>
+';
+
+$lang["sale-meta"] = '
+<meta name="description" content="{$cnum} Houses for sale in {$location}{$postcode}.The best properties and homes for sale in {$location} you find here." />
+<meta name="keywords" content="Houses for sale in {$location}, {$location}, Flats in {$location}, Houses for sale & to rent, Properties for sale in {$location}" />
+<meta name="geo.placename" content="{$placename}" />
+<meta name="geo.position" content="{$lat};{$lng}" />
+<meta name="ICBM" content="{$lat}, {$lng}" />
+<meta property=”og:title” content=”House for Sale in {$location}”/>
+<meta property=”og:type” content=”website”/>
+<meta property=”og:description” content=”{$cnum} Houses for sale in {$location}{$postcode}. The best properties and homes in {$location} you find here.”/>
+';
+
+$lang["rent-meta"] = '
+<meta name="description" content="{$cnum} Houses to rent in {$location}. The best properties and homes to rent in {$location} you find here." />
+<meta name="keywords" content="Houses to rent in {$location}, Flats in {$location}, Houses for sale & to rent, Properties to rent in {$location}" />
+<meta name="geo.placename" content="{$placename}" />
+<meta name="geo.position" content="{$lat};{$lng}" />
+<meta name="ICBM" content="{$lat}, {$lng}" />
+<meta property=”og:title” content=”House for Sale & to Rent in {$location}”/>
+<meta property=”og:type” content=”website”/>
 <meta property=”og:description” content=”{$cnum} Houses for sale in {$location}{$postcode}, Houses to rent in {$location}. Best Properties in {$location} you find here.”/>
 ';
+
+
 
 $lang["paginator-firstpage"] = "First Page";
 $lang["paginator-pagatitle"] = 'Houses {$type} in {$query} page {$name}'; 
