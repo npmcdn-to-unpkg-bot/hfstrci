0d327475367a43fb6deb9c66fdf29fcb7f0cb1d6
felp302 <felp302@gmail.com>
2015-09-26 16:20:53 +0100

    Update houses.php

--- application/controllers/houses.php
+++ application/controllers/houses.php
@@ -56,7 +56,7 @@ class Houses extends CI_Controller {
 			return $res;
 			
 		}else{
-			$url = 'http://maps.googleapis.com/maps/api/geocode/json?address='.$search_code.'+United+Kingdom&sensor=false&key=AIzaSyAhqHhs7yOY46r2H-71JhTA8dGorPqIu30';
+			$url = 'https://maps.googleapis.com/maps/api/geocode/json?address='.$search_code.'+United+Kingdom&sensor=false&key=AIzaSyAhqHhs7yOY46r2H-71JhTA8dGorPqIu30';
 			$json = json_decode(file_get_contents($url));
 			
 			if(isset($json->results[0])){
