8efb7b1a31194aebb34721fa1f5806d5ecb4b8cb
felp302 <felp302@gmail.com>
2016-05-03 00:01:51 +0100

    Update lists.php

--- application/views/citylight/lists.php
+++ application/views/citylight/lists.php
@@ -179,25 +179,26 @@
                   </div>
               
                 <!-- END FIND PROPERTY -->
-                <div id="contentad156278"></div>
-		<script type="text/javascript">
-		    (function(d) {
-		        var params =
-		        {
-		            id: "ceb803c4-5110-4605-8eed-258ff6c95049",
-		            d:  "aG91c2VzZm9yc2FsZXRvcmVudC5jby51aw==",
-		            wid: "156278",
-		            cb: (new Date()).getTime()
-		        };
-		
-		        var qs=[];
-		        for(var key in params) qs.push(key+'='+encodeURIComponent(params[key]));
-		        var s = d.createElement('script');s.type='text/javascript';s.async=true;
-		        var p = 'https:' == document.location.protocol ? 'https' : 'http';
-		        s.src = p + "://api.content.ad/Scripts/widget2.aspx?" + qs.join('&');
-		        d.getElementById("contentad156278").appendChild(s);
-		    })(document);
-		</script>
+               <script type='text/javascript'>
+<!--//<![CDATA[
+   document.MAX_ct0 ='';
+   var m3_u = (location.protocol=='https:'?'https://cas.criteo.com/delivery/ajs.php?':'http://cas.criteo.com/delivery/ajs.php?');
+   var m3_r = Math.floor(Math.random()*99999999999);
+   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+   document.write ("zoneid=412356");document.write("&amp;nodis=1");
+   document.write ('&amp;cb=' + m3_r);
+   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+   document.write ("&amp;loc=" + escape(window.location).substring(0,1600));
+   if (document.context) document.write ("&context=" + escape(document.context));
+   if ((typeof(document.MAX_ct0) != 'undefined') && (document.MAX_ct0.substring(0,4) == 'http')) {
+       document.write ("&amp;ct0=" + escape(document.MAX_ct0));
+   }
+   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+   
+   document.write ("'></scr"+"ipt>");
+//]]>--></script>
+
                 
                 <?php if($lat && $lng){ 
 	                echo '<div class="block-sidebar">
