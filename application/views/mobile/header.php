a80beca8d57ec2fa6f6a62c8ffd3aab1a1f89127
Felipe 3 <tomaszczech@michael-riiss-iMac-3.local>
2016-08-05 13:34:38 +0100

    fix lead

--- application/views/mobile/header.php
+++ application/views/mobile/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html><html><head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link rel="stylesheet" type="text/css" href="<?php echo $this->config->base_url(); ?>singlehome.min.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $this->config->base_url(); ?>css/singlehome.min.css" />
 <link rel="shortcut icon" href="<?php echo $this->config->base_url(); ?>images/icon/favicon.ico" type="image/x-icon">
 <link rel="icon" href="<?php echo $this->config->base_url(); ?>images/icon/favicon.ico" type="image/x-icon">
 <title><?php echo $title; ?></title>
