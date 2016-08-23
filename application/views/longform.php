ff03169e646c9900e5597814d04476411117d9c9
felp302 <felp302@gmail.com>
2016-08-23 10:11:57 +0100

    ads

--- application/views/longform.php
+++ application/views/longform.php
@@ -2,7 +2,7 @@
         <h1><a href="<?php echo $this->config->base_url(); ?>" title="HOUSES for SALE & to RENT"><img src="<?php echo $this->config->base_url(); ?>images/houses-for-sale-to-rent.png" alt="HOUSES for SALE & to RENT" title="HOUSES for SALE & to RENT"/></a></h1>
     </header>
 
-<?php /*    <center>
+ <center>
 <script type='text/javascript'>
 <!--//<![CDATA[
    if (!window.AdButler){(function(){var s = document.createElement("script"); s.async = true; s.type = "text/javascript";s.src = 'http://ab168091.adbutler-tachyon.com/app.js';var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(s, n);}());}
@@ -12,7 +12,7 @@
     document.write('<'+'div id="placement_206595_'+plc206595+'"></'+'div>');
     AdButler.ads.push({handler: function(opt){ AdButler.register(168091, 206595, [728,90], 'placement_206595_'+opt.place, opt); }, opt: { place: plc206595++, keywords: abkw, domain: 'ab168091.adbutler-tachyon.com', click:'CLICK_MACRO_PLACEHOLDER' }});
 //]]>-->
-</script></center> */ ?>
+</script></center> 
 
     <?php if(isset($message))
                 echo '<div class="warning">'.$message.'</div>';
